58570114ea499c00318857284b145d9bb4e91030
Y Sung <spero61@gmail.com>
2023-10-24 18:45:00 +0900

    implement app

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,4 +1,10 @@
 export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "amplifytripsplanner": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string"
+    }
+  },
   "auth": {
     "amplifytripsplanner6641f990": {
       "AppClientID": "string",
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
+import 'package:amplify_api/amplify_api.dart';
 import 'package:amplify_auth_cognito/amplify_auth_cognito.dart';
 import 'package:amplify_flutter/amplify_flutter.dart';
+import 'package:amplify_trips_planner/models/ModelProvider.dart';
 import 'package:amplify_trips_planner/trips_planner_app.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -24,6 +26,7 @@ Future<void> main() async {
 Future<void> _configureAmplify() async {
   await Amplify.addPlugins([
     AmplifyAuthCognito(),
+    AmplifyAPI(modelProvider: ModelProvider.instance),
   ]);
   await Amplify.configure(amplifyconfig);
 }
