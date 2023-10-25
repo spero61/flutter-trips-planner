bf98119ac976d02c6edd32f642f2e77c79cbbf6c
Y Sung <spero61@gmail.com>
2023-10-25 10:17:05 +0900

    add auth

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -15,5 +15,11 @@ export type AmplifyDependentResourcesAttributes = {
       "UserPoolId": "string",
       "UserPoolName": "string"
     }
+  },
+  "storage": {
+    "s3998489ce": {
+      "BucketName": "string",
+      "Region": "string"
+    }
   }
 }
